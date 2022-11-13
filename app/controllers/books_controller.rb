68d535b4e91d5ef15b83b5cea4052809f66542aa
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-11-13 09:11:02 +0000

    add_CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,6 @@
 class BooksController < ApplicationController
   
-  def new
-    @book=Book.new
-  end
+  
   
   def create
     book = Book.new(book_params)
@@ -11,13 +9,28 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books=book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
